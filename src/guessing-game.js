11696a4f2d0400cb5fcdc7c515335b4f08815b07
Dimiricus <getdimiricus@gmail.com>
2021-08-01 00:07:02 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,70 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.result;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
 
+        this.result = this.min + Math.ceil( (this.max - this.min) / 2 );
+
+        return this.result;
     }
 
     lower() {
 
+        this.max = this.result;
     }
 
     greater() {
 
+        this.min = this.result;
+        
     }
 }
 
 module.exports = GuessingGame;
+
+
+
+
+/* 
+guess() {
+
+        if ( (this.max - this.min) % 2 == 0 ) {
+
+            this.result = ( this.min + (this.max - this.min) / 2 ) + 1;
+
+        } else {
+
+            this.result = this.min + Math.round((this.max - this.min) / 2 );
+        }
+        
+        return this.result;
+    }
+
+    lower() {
+
+        this.max = this.min + Math.floor( (this.max - this.min) / 2 );
+        
+    }
+
+    greater() {
+
+        if ( (this.max - this.min) % 2 == 0 ) {
+
+            this.min = ( this.min + (this.max - this.min) / 2 ) + 1;
+
+        } else {
+
+            this.min = this.min + Math.ceil( (this.max - this.min) / 2 );
+        }
+        
+    }
+    */
